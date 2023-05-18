33248ec97d1720e67bf6b1db3055fba66727a47d
Novan01 <patrick@casaanderson.net>
2023-05-18 18:10:14 -0500

    new file:   src/main/java/com/rpg/environments/Tavern.java 	deleted:    characterDatabase.json 	modified:   pom.xml 	deleted:    src/main/java/com/rpg/CharacterCreator.java 	modified:   src/main/java/com/rpg/MainMenu.java 	modified:   src/main/java/com/rpg/RPG_Main.java 	deleted:    src/main/java/com/rpg/Tavern.java 	modified:   src/main/java/com/rpg/character_database/CharacterSelector.java 	modified:   src/main/java/com/rpg/character_database/characterDatabase.java 	deleted:    target/classes/com/rpg/CharacterCreator$1.class 	deleted:    target/classes/com/rpg/CharacterCreator$2.class 	deleted:    target/classes/com/rpg/CharacterCreator$3.class 	deleted:    target/classes/com/rpg/CharacterCreator.class 	modified:   target/classes/com/rpg/MainMenu$2.class 	modified:   target/classes/com/rpg/RPG_Main.class 	deleted:    target/classes/com/rpg/Tavern.class 	modified:   target/classes/com/rpg/character_database/CharacterSelector$1.class 	modified:   target/classes/com/rpg/character_database/CharacterSelector$2.class 	modified:   target/classes/com/rpg/character_database/CharacterSelector$3.class 	modified:   target/classes/com/rpg/character_database/CharacterSelector$4.class 	modified:   target/classes/com/rpg/character_database/CharacterSelector.class 	modified:   target/classes/com/rpg/character_database/characterDatabase.class

---
+++ src/main/java/com/rpg/environments/Tavern.java
@@ -0,0 +1,33 @@
+package com.rpg.environments;
+
+import java.util.Scanner;
+import com.rpg.character_classes.Character;
+import com.rpg.npc_characters.TavernKeeper;
+
+//import java.util.ArrayList;
+
+public class Tavern {
+
+	public void enterTavern(Character player) {
+		
+	}
+
+	public void printMenu(String[] menu) {
+		
+	}
+
+	public void selectItem(String[] menu, Character player) {
+		
+
+	}
+
+	public void barKeeperDialouge(Character player, String[] drinks, String[] food) {
+		
+
+	}
+
+	public void tavernKeeperOptions(Character player, String[] drinks, String[] food) {
+		
+	}
+
+}
