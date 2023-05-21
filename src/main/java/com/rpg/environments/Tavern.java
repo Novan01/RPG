015147c3ba52b6bd5cb5e1a547b39352683fa756
Novan01 <patrick@casaanderson.net>
2023-05-21 18:54:46 -0500

    modified:   pom.xml 	modified:   src/main/java/com/rpg/environments/Tavern.java 	modified:   target/classes/com/rpg/environments/Tavern.class

--- src/main/java/com/rpg/environments/Tavern.java
+++ src/main/java/com/rpg/environments/Tavern.java
@@ -4,29 +4,13 @@ import java.util.Scanner;
 import com.rpg.character_classes.Character;
 import com.rpg.npc_characters.TavernKeeper;
 
+
+
 //import java.util.ArrayList;
 
 public class Tavern {
 
 	public void enterTavern(Character player) {
-		
-	}
-
-	public void printMenu(String[] menu) {
-		
-	}
-
-	public void selectItem(String[] menu, Character player) {
-		
-
-	}
-
-	public void barKeeperDialouge(Character player, String[] drinks, String[] food) {
-		
-
-	}
-
-	public void tavernKeeperOptions(Character player, String[] drinks, String[] food) {
 		
 	}
 
